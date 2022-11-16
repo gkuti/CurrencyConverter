b08082298153ce72289105e724dc91743744f767
gkuti <gamikoflife@gmail.com>
2022-11-16 19:49:12 +0000

    Enable back navigation with action bar

--- app/src/main/java/com/gamik/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/gamik/currencyconverter/MainActivity.kt
@@ -2,7 +2,9 @@ package com.gamik.currencyconverter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,5 +17,10 @@ class MainActivity : AppCompatActivity() {
         val defaultHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
         val navController = (defaultHostFragment as NavHostFragment).navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, AppBarConfiguration(navController.graph))
     }
 }
