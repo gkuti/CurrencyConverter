a03579c0e18a8ac33525bc7e05bcc226ed738ba9
gkuti <gamikoflife@gmail.com>
2022-11-16 19:45:36 +0000

    Implement rate history
    - Add 3 day rates
    - Add other currencies view

---
+++ app/src/main/java/com/gamik/currencyconverter/ui/history/HistoryFragment.kt
@@ -0,0 +1,73 @@
+package com.gamik.currencyconverter.ui.history
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
+import com.gamik.currencyconverter.R
+import com.gamik.currencyconverter.databinding.FragmentHistoryBinding
+import com.gamik.currencyconverter.util.DateUtil
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class HistoryFragment : Fragment(R.layout.fragment_history) {
+    private lateinit var binding: FragmentHistoryBinding
+    private val viewModel: HistoryViewModel by viewModels()
+    private val args: HistoryFragmentArgs by navArgs()
+    private lateinit var rates: Map<String, Map<String, String>>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHistoryBinding.bind(view)
+        historyObserver()
+        viewModel.getHistory(
+            DateUtil.threeDaysAgo(),
+            DateUtil.today(),
+            args.baseCurrency,
+            "${args.targetCurrency}, ${args.symbols.toList().joinToString { it }}"
+        )
+    }
+
+    private fun historyObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.historyViewState.collect { viewState ->
+                    when (viewState) {
+                        is HistoryViewState.Success -> {
+                            rates = viewState.response.rates
+                            showHistory()
+                            println(viewState.response)
+                        }
+                        HistoryViewState.Error -> {
+
+                        }
+                        HistoryViewState.Loading -> {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showHistory() {
+        val todayRate = rates[DateUtil.today()]
+        val otherCurrencies =
+            todayRate?.keys?.filterNot { it == args.targetCurrency }
+                ?.joinToString("\n") {
+                    "$it: ${todayRate[it]}"
+
+                }
+        val threeDayRate =
+            rates.keys.joinToString("\n") {
+                "$it: ${rates[it]?.get(args.targetCurrency)}"
+            }
+        binding.threeDayRate.text = threeDayRate
+        binding.otherCurrencies.text = otherCurrencies
+    }
+}
